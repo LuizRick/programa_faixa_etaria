7fb1a12288f49b6bcdeba456a5da203c3d856b6c
unknown <luizhenrique.silva@certifica.com.br>
2025-01-31 17:43:47 -0300

    criado código brincando numero_secreto

---
+++ brincando.py
@@ -0,0 +1,23 @@
+import random
+
+numero_secreto = [random.randint(1,10)]
+tentativa = 0
+tentativas = 4
+while tentativa <= tentativas:
+    chute = int(input ('Informe um número de 1 a 10: '))
+    print(f'O número escolhido foi {chute}!')
+
+    if tentativa == 4:
+        print(f'Fim das tentativas! O número secreto era {numero_secreto}!')
+    
+    elif chute == numero_secreto:
+        print(f'Parabéns você acertou o número secreto: {chute}!')
+    elif chute != numero_secreto:
+        print('Que pena, você errou! :(')
+    tentativa += 1
+    
+
+'''if tentativa == tentativas:
+    print(f'Fim das tentativas! O número secreto era {numero_secreto}!')
+     '''
+
